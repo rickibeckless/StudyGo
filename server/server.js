72adbeae7d02be30e31cc4e937002e31044fa743
Ricki Beckless <glbeckless@hotmail.com>
2024-09-22 22:39:23 -0500

    README

--- server/server.js
+++ server/server.js
@@ -1,16 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-import path from 'path';
-import { fileURLToPath } from 'url';
+
 import classesRoutes from './routes/classesRoutes.js';
 import subjectsRoutes from './routes/subjectsRoutes.js';
 import urlRoutes from './routes/urlRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 app.use(cors());
 
@@ -19,10 +15,8 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use('/public', express.static(path.join(__dirname, '../client/public')));
-
-app.use('/css', express.static(path.join(__dirname, '../client/css')));
-app.use('/scripts', express.static(path.join(__dirname, '../client/public/scripts')));
+app.use('/public', express.static('public'));
+app.use('/scripts', express.static('scripts'));
 
 app.use('/api/classes', classesRoutes);
 app.use('/api/subjects', subjectsRoutes);
